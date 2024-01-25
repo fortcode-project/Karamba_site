cd9f7c5c540c8fc9dd757eaf6c8da1049f8ba7bd
fortcodeproject <pachecofortcodedig0@gmail.com>
2024-01-25 18:15:49 +0100

    Fix(Website - Bilhetes) - Criação de Formulario

--- resources/views/pages/bilhete.blade.php
+++ resources/views/pages/bilhete.blade.php
@@ -7,6 +7,12 @@
             <h1 class="text-center text-uppercase">
                 Venda de Bilhetes, Brevemente...
             </h1>
+            <form action="">
+                <div class="form-group">
+                    <label for="" class="form-label">Nome: </label>
+                    <input type="text" class="form-control" placeholder="Insira o seu nome...">
+                </div>
+            </form>
         </main>
         @include("components.contact")
     @include("components.footer")
