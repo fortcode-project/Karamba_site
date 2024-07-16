0446e072fbbd3adb00e95f141719d36dc01a2145
fortcodeproject <pachecofortcodedig0@gmail.com>
2024-07-16 17:00:15 +0100

    Fixe(Correção de conflitos)

--- resources/views/layouts/site/app.blade.php
+++ resources/views/layouts/site/app.blade.php
@@ -3,18 +3,13 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
   <title>@yield("title")</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords" class="">
-
   <link rel="shortcut icon" type="image/png" href="{{asset("site/assets/img/logo.ico")}}">
 
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Amatic+SC:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="./chat_xandria/style.css">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -34,8 +29,7 @@
 <body>
 
     @yield("content")
-
-<<<<<<< HEAD
+    
       {{-- chat --}}
       <div class="chatbot-container" id="chatbot-container">
         <div class="chatbot-header">
@@ -56,28 +50,6 @@
     <button id="chatbot-button"><img width="40" src="{{asset("chat_xandria/icone.png")}}" alt=""></button>
 
     <script src="{{asset("chat_xandria/script.js")}}"></script>
-=======
-    {{-- chat --}}
-    <div class="chatbot-container" id="chatbot-container">
-      <div class="chatbot-header">
-          <div class="" style="display: flex;gap:5px;align-items: center;">
-              <img width="30" src="../chat_xandria/icone.png" alt="">
-              <h3>Xándria</h3>
-          </div>
-          <button id="close-chatbot">X</button>
-      </div>
-      <div class="chatbot-body" id="chatbot-body">
-          <div class="message bot-message">Olá! Como posso ajudar você hoje?</div>
-      </div>
-      <div class="chatbot-footer">
-          <input type="text" id="user-input" placeholder="Digite sua mensagem...">
-          <button id="send-message"><img width="20" src="../chat_xandria/send.png" alt=""></button>
-      </div>
-  </div>
-  <button id="chatbot-button"><img width="40" src="./chat_xandria/icone.png" alt=""></button>
-
-  <script src="./chat_xandria/script.js"></script>
->>>>>>> 9bc5980f3d105f9037a19ae1e4d8dde94e529295
 
     <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
     <a href="{{route("loja.get.cart.total")}}" style="color: #fff;background: #F4C400;" class="cartcout d-flex align-items-center justify-content-center">      
@@ -85,6 +57,18 @@
         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l.5 2H5V5zM6 5v2h2V5zm3 0v2h2V5zm3 0v2h1.36l.5-2zm1.11 3H12v2h.61zM11 8H9v2h2zM8 8H6v2h2zM5 8H3.89l.5 2H5zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
       </svg>
     </a>
+    <style>
+      .cartcout {
+  position: fixed;
+  right: 130px;
+  bottom: 25px;
+  z-index: 99;
+  width: 60px;
+  height: 60px;
+  border-radius: 50px;
+  transition: all 0.4s;
+}
+    </style>
     
     <div id="preloader"></div>
   
